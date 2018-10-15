e0a03b4e378b606add8da259bd105a05f49178c6
anthony-mendola <anthonymendola@gmail.com>
2018-10-14 22:07:53 -0400

    palindrome solution

---
+++ palindrome.js
@@ -0,0 +1,16 @@
+//Solution 1
+function palindrome(str) {
+  const reversed = str
+    .split("")
+    .reverse()
+    .join("");
+
+  return str === reversed;
+}
+
+//Solution 2
+// function palindrome(str) {
+//   return str.split("").every((char, i) => {
+//     return char === str[str.length - i - 1];
+//   });
+// }
